b05a2f85c0b1334d4cf537edd9db479116200a1f
danilshatalov <danilshatalov96@gmail.com>
2023-01-16 21:34:45 +0200

    Add hw

--- Program.cs
+++ Program.cs
@@ -1,61 +1,66 @@
 ﻿// Задача 1
-// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (Задачи, решенные через Maath.Pow, будут отправлены на переделку, так как задача стоит в том, чтобы написать цикл)
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.WriteLine("Введите число A: ");
-int A = int.Parse(Console.ReadLine());
+int[] firstArray = getArray(4, 100, 1000);
+System.Console.WriteLine($"Первый массив: [{String.Join(", ", firstArray)}]");
+System.Console.WriteLine($"Количество четных чисел в этом массиве: {isEvenNumb(firstArray)}");
 
-Console.WriteLine("Введите число B: ");
-int B = int.Parse(Console.ReadLine());
+// Задача 2
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях. (позиции - это индексы, нечетные индексы это 1,3,5 и тд)
 
-Console.WriteLine($"Число {A} в степени {B} ровняется {getPow(A, B)}");
+int[] secondArray = getArray(10, 0, 10);
+System.Console.WriteLine($"Второй массив: [{String.Join(", ", secondArray)}]");
 
-// Задача 2 
-// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-
-Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine($"Cумма цифр в числе {num} получается {getSumNumb(num)}");
-
-// Задача 3
-// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
-
-int[] Array = getRandomArray();
-Console.Write($"[{String.Join(";", Array)}]");
-
-/////////////-------------------Методы----------------------//////////////
-
-int getPow(int x, int n)
+int sum = 0;
+for (int i = 0; i < secondArray.Length; i++)
 {
-    int sum = 1;
-    for (int i = n; i > 0; i--)
+    if (i % 2 == 0) continue;
+    else
     {
-        sum = sum * x;
+        sum += secondArray[i];
     }
-    return sum;
 }
 
-int getSumNumb(int x)
+System.Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
+
+// Задача 3 
+// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] thirdArray = getArray(6, 1, 6);
+System.Console.WriteLine($"Третий массив: [{String.Join(", ", thirdArray)}]");
+
+
+for (int i = thirdArray.Length - 1, n = 0; (i >= thirdArray.Length / 2 && n < thirdArray.Length / 2); i--, n++)
 {
-    int sum = 0;
-    while (x != 0)
-    {
-        sum = sum + x % 10;
-        x = x / 10;
-    }
-    return sum;
+    int firstEl = thirdArray[i];
+    int secondEl = thirdArray[n];
+    int result = firstEl * secondEl;
+    System.Console.WriteLine($"Последний элемент {firstEl} и первый элемент {secondEl}, а также результат {result}");
 }
 
-int[] getRandomArray()
+
+///--------Методы--------//////
+int[] getArray(int size, int minValue, int maxValue)
 {
-    int[] array = new int[8];
-    for (int i = 0; i < 8; i++)
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(0, 1000);
+        array[i] = new Random().Next(minValue, maxValue);
     }
     return array;
+}
+
+int isEvenNumb(int[] array)
+{
+    int counter = 0;
+    foreach (int arrayItem in array)
+    {
+        if (arrayItem % 2 == 0)
+        {
+            counter += 1;
+        }
+    }
+    return counter;
 }
